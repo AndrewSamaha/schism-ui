6386c461da216ba67e626ce185bb5cdbaa0a03a3
Andrew Samaha <andrew.samaha@gmail.com>
2022-04-15 17:26:36 -0400

    login gets and sets auth token

--- src/index.js
+++ src/index.js
@@ -5,10 +5,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('authorization');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : "",
+    }
+  }
+});
+
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
--- src/stories/organisms/CredentialCard/CredentialCard.jsx
+++ src/stories/organisms/CredentialCard/CredentialCard.jsx
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 import './CredentialCard.css';
 import TextField from '@mui/material/TextField';
 import Container from '@mui/material/Container';
 import { Button } from '../../atoms/Button/Button';
 
-export const CredentialCard = () => (
+const LOGIN = gql`
+# Increments a back-end counter and gets its resulting value
+mutation login($name: String!, $password: String!) {
+  login(name: $name, password: $password) {
+    ... on Player {
+      id
+      name
+      authToken
+    }
+    ... on ErrorAuthentication {
+      message
+    }
+  }
+}
+`;
+
+export const CredentialCard = () => {
+  const [login, { data, loading, error }] = useMutation(LOGIN, {onCompleted: (data) => {
+    localStorage.setItem('authorization',data.login.authToken);    
+    console.log({data})
+  }});
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (loading) return 'Submitting...';
+  if (error) return `Submission error! ${error.message}`;
+  if (data) return `${data}`;
+
+  return (
   <Container maxWidth="sm" className="credential">
-    
-    <TextField id="username" label="Username" variant="outlined" /><br/>
-    <TextField id="password" label="Password" variant="outlined" type="password" /><br/>
-    <Button label="Login"/>
+      <TextField id="name" label="Username" variant="outlined" onChange={(event) => setName(event.target.value)} /><br/>
+      <TextField id="password" label="Password" variant="outlined" type="password" onChange={(event) => setPassword(event.target.value)} /><br/>
+      <Button label="Login" onClick={() => {
+          console.log('sending login');
+          login({ variables: { name, password } });
+          console.log({data});
+        }} />
+    data: {data}<br/>
+    loading: {loading}<br/>
+    error: {error}<br/>
   </Container>
-);
+  );
+}
 
 CredentialCard.propTypes = { };
 
