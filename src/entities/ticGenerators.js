88ef2cbf3b6fcd3bdaf2c0a66043cb18dd47ae3b
Andrew Samaha <andrew.samaha@gmail.com>
2022-10-17 21:23:10 -0400

    feat: updating entity.position during move

--- src/entities/ticGenerators.js
+++ src/entities/ticGenerators.js
@@ -39,13 +39,15 @@ const createEntityTicGenerator = ({entity, worldLocation, totalTime}) => {
 
 // the first action generator
 const straightLineMoveTicGenerator = ({entity, worldLocation}) => {
-    return (ref, delta) => {
+    return (ref, delta, entityReducer) => {
         const length = entity.speed * delta;
         const { current } = ref;
         const { angle, dist } = getAngleDist(current.position, worldLocation);  
         if (dist < length) {
             const new3DPosition = new THREE.Vector3(worldLocation.x, worldLocation.y, worldLocation.z); 
             current.position.set(new3DPosition.x, new3DPosition.y, new3DPosition.z);
+            entity.position[0] = current.position.x;
+            entity.position[1] = current.position.y;
             entity.tic = null;
             return;
         }
@@ -53,6 +55,9 @@ const straightLineMoveTicGenerator = ({entity, worldLocation}) => {
         step.rotateAround({x: 0, y: 0}, angle);
         const step3D = new THREE.Vector3(step.x, step.y, 0);
         current.position.add(step3D);
+        console.log(current.position)
+        entity.position[0] = current.position.x;
+        entity.position[1] = current.position.y;
     }
 }
 
