7795b375f7a7827045da098389e7084ff28df660
Andrew Samaha <andrew.samaha@gmail.com>
2022-10-25 21:34:42 -0400

    feat: WIP entityMutationRequest

--- src/entities/entityMutationRequest.js
+++ src/entities/entityMutationRequest.js
@@ -5,6 +5,32 @@ const applyChangesToEntityState = (state, changes) =>
     changes.forEach((change) => set(state, change.path, change.value));
 
 const actionEffect = (args) => {
+    /*
+        - First call, 
+            args:
+                changes - array of objects containing path (e.g., a lodash/set path) and value
+                sourceEntity - reference to the sourceEntity
+                targetEntity - reference to the targetEntity
+                actionDetails - an object containiner action.longName and maybe other things
+            returns:
+                a parent actionEffect object with these fields:
+                    id (a uuid)
+                    parent: null (null indicates this is a parent)
+                    startTime: the time the actionEffect was created
+                    sourceEntity + sourceEntityJSON + sourceEntityId
+                    targetEntity + targetEntityJSON + targetEntityId
+                    actionDetails
+                    changeLog: an array of objects containiner time, timeDelta (the time since
+                               the start of the actionEffect), and the changes since the last
+                               actionEffect
+                    applyAll: a function that applies all the changes in the change log to a
+                                target entity
+        - Second call,
+            args:
+                parent: the parent actionEffect
+                changes: an array of path and value fields since the last changeEffect
+        
+    */
     const { parent, changes } = args;
 
     if (parent) {
@@ -44,4 +70,8 @@ const entityMutationRequest = (args) => {
     const { sourceEntityId, targetEntityId, actionDetails, changes } = args;
     const startTime = Date.now();
 
+}
+
+export {
+    actionEffect
 }
