c8de4520b83ac6357f6ff790a519bdb1ed52680b
Andrew Samaha <andrew.samaha@gmail.com>
2022-12-30 20:53:59 -0500

    chore(scripts): script to copy models from schism-assets to /public

---
+++ scripts/importAssets.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+var path = require('path')
+const fse = require('fs-extra')
+const fs = require('fs')
+
+const ASSETS_FOLDER = './public/assets';
+
+console.log('importAssets')
+console.log(process.env.SCHISM_ASSET_FOLDER)
+
+const fileExtensionsToCopy = [
+    '.gltf'
+]
+
+const foldersToCopy = [
+    'buildings',
+    'character',
+    'resources'
+]
+
+const filterFunc = (src, dest) => {
+    const copyTest = (src, dest) => {
+        if (path.basename[0] === '.') return false;                         // don't copy hidden files/folders
+        if (fs.lstatSync(src).isDirectory()) return true;                   // do copy directories
+        if (fileExtensionsToCopy.includes(path.extname(src))) return true;  // only copy thes file extensions
+        return false;
+    }
+    const copyResult = copyTest(src, dest)
+    if (!copyResult) return copyResult;
+    console.log(src, ' -> ', dest)
+    
+    return copyResult;
+}
+
+console.log('removing existing assets folder ', ASSETS_FOLDER)
+fs.rmSync(ASSETS_FOLDER, { recursive: true, force: true });
+
+console.log('copying assets from schism-assets ', process.env.SCHISM_ASSET_FOLDER)
+foldersToCopy.forEach((folder) => {
+    fse.copySync(`${process.env.SCHISM_ASSET_FOLDER}/${folder}`, `${ASSETS_FOLDER}/${folder}`, {
+        filter: filterFunc
+    })
+})
