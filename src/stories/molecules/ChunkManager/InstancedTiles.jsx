844b7cbb555eb93d96ff9742106a231fd4f640c8
Andrew Samaha <andrew.samaha@gmail.com>
2023-03-24 06:46:32 -0400

    refactor(instancedtiles): pOC InstancedTiles working - still need to map from tiledata

--- src/stories/molecules/ChunkManager/InstancedTiles.jsx
+++ src/stories/molecules/ChunkManager/InstancedTiles.jsx
@@ -25,15 +25,16 @@ import { ViewGeometry, ViewRotation } from '../../../constants/viewport';
 import { CHUNK_SIZE } from '../../../constants/tileChunks';
 import { QUERY_SERVER_FOR_CHUNKS, SHOW_VIEWPORT_BOUNDARY, USE_INSTANCED_MESH_TEST } from '../../../constants/dev';
 
-const xMax = 100;
-const yMax = 100;
-const tiles = new THREE.Matrix4();
+const xMax = 30;
+const yMax = 30;
+const tiles = new THREE.Object3D();
+const tilesGeometry = new THREE.BoxBufferGeometry(.9, .9, 0);
+const material = new THREE.MeshLambertMaterial({ color: "pink" });    
 
 export const InstancedTiles = ({gameReducer, userReducer, worldStateQuery, children, client }) => { // chunkQuery,
-
-    const ref = useRef()
-    let tiles;
-
+    
+    const ref = useRef();
+    
     useEffect(() => {
         // tiles = 
         let counter = 0;
@@ -47,14 +48,33 @@ export const InstancedTiles = ({gameReducer, userReducer, worldStateQuery, child
                 ref.current.setMatrixAt(id, tiles.matrix);
             }
         }
+        console.log(`created ${counter} tiles`)
+
       ref.current.instanceMatrix.needsUpdate = true;
     }, []);
+    
+    // useFrame(({ clock }) => {
+    //   let counter = 0;
+    //     // ref.current.setMatrixAt(0, new THREE.Matrix4())
+    //     for (let x = 0; x < xMax; x++) {
+    //         for (let y = 0; y < yMax; y++) {
+    //             const id = counter++;
+    //             tiles.position.set(xMax / 2 - x, yMax / 2 - y, 0);
+    //             // tiles.rotation.y = t;
+    //             tiles.updateMatrix();
+    //             ref.current.setMatrixAt(id, tiles.matrix);
+    //         }
+    //     }
+    //     // console.log(`created ${counter} tiles`)
+
+    //   ref.current.instanceMatrix.needsUpdate = true;
+    // })
 
       return (
-        <instancedMesh ref={ref} args={[null, null, 1]}>
-          <planeGeometry args={[1, 1]} />
+        <instancedMesh ref={ref} args={[null, null, xMax * yMax]}>
+          <planeGeometry args={[.9, .9]} />
           <meshNormalMaterial color='pink' />
-          {children}
+          {/* {children} */}
         </instancedMesh>
       )
   }
